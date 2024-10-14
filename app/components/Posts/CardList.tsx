cb2833d3c7efaa7c7dea78b69d8382e4f511de64
Nims_ <134251429+nimas123@users.noreply.github.com>
2024-10-14 21:21:37 +0700

    Update CardList.tsx

--- app/components/Posts/CardList.tsx
+++ app/components/Posts/CardList.tsx
@@ -1,10 +1,15 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const CardList = ({ children }: any) => {
-  return (
-      <div className="bg-[tomato] w-full p-4">
-          {children}
-      </div>
-  )
+interface CardListProps {
+  children: React.ReactNode;
 }
 
-export default CardList
+const CardList = ({ children }: CardListProps) => {
+  return (
+    <div className="bg-[tomato] w-full p-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default CardList;
